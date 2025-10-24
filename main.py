70f88eead4578f4ce3cb98c9ec35c7f23c1900c5
Lkshjnj <lakshyajuneja22@gmail.com>
2025-10-24 07:38:42 +0530

    output polished and report generation is done, non-alphabetic characters are successfully skipped

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from stats import count_words_and_characters
+from stats import count_words_and_characters, char_for_report
+import sys
 
 def main():
-    word_count, char_count = count_words_and_characters("./books/frankenstein.txt")
-    print(f"found {word_count} in the text\n")
-    print(f"the character count is as follows \n {char_count}")
 
+    if len(sys.argv) != 2:
+        print("[-] Usage: python3 main.py <filepath>")
+        sys.exit(1)
+
+    print("==================== BOOKBOT =========================\n\n")
+    print("==================== Word Count ======================\n")
+    word_count, char_count = count_words_and_characters(sys.argv[1])
+    print(f"found {word_count} in the text\n")
+    # print(f"the character count is as follows \n {char_for_report(char_count)}")
+    print("====================== Character Count ========================")
+    for item in char_for_report(char_count):
+        if item['char'].isalpha():
+            print(f"{str(item["char"])} : {str(item["num"])}")
+    print("======================= END ============================")
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -8,4 +8,11 @@ def count_words_and_characters(filepath: str):
         char_dict[char] = char_dict.get(char, 0) + 1
 
     return len(word_list), char_dict    
-        
+
+def sort_on(items):                    # helper function to sort the list of dictionaries returned by char_and_report function.
+    return items['num']
+
+def char_for_report(char_dict: dict):
+    organized_list = [{'char': key, 'num': value} for key, value in char_dict.items()]
+    organized_list.sort(reverse=True, key=sort_on)
+    return organized_list
